9e7b3863c2989cb1632032a951d7f62799c6da89
jorge <esgerry81@gmail.com>
2024-09-22 20:41:41 -0600

    Se creo el componente Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,26 @@
+import {categories} from "../data/categories"
+
 export default function Form() {
   return (
-    <div>Form</div>
+    <form
+      className="space y-5 bg-white shadow p-10 rounded-lg"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoria</label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        > 
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}>
+              {category.name}
+            </option>
+          ))} 
+        </select>
+      </div>
+
+    </form>
   )
 }
