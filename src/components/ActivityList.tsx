10c2b59c2b0ce5308f94ab6beb61accdd5be8ea1
jorge <ogomez.isc@gmail.com>
2024-09-24 11:55:42 -0600

    Formateando la información que se añade al listado de Actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,12 +1,15 @@
 import { Activity } from "../types"
-
+import { categories } from "../data/categories"
+import { useMemo } from "react"
 type ActivityListProps = {
     activities: Activity[]
 }
 
 export default function ActivityList({activities} : ActivityListProps) {
     
-    console.log(activities)
+   const categoryName = useMemo(() => 
+    (category: Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
+   , [activities]) 
 
     return (
         <>
@@ -15,8 +18,8 @@ export default function ActivityList({activities} : ActivityListProps) {
             {activities.map( activity => (
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative">
-                        <p>
-                            {activity.category}
+                        <p className={`absolute-top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                            {categoryName(+activity.category)}
                         </p>
                         <p className="text-2xl font-bold pt-5">{activity.name}</p>
                         <p className="font-black text-4xl text-lime-500">
