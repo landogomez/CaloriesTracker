96c3c928c5e6d89455128f23001e4ad184efba95
jorge <ogomez.isc@gmail.com>
2024-09-24 12:01:38 -0600

    Se agrego un boton para editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,6 +1,10 @@
 import { Activity } from "../types"
 import { categories } from "../data/categories"
 import { useMemo } from "react"
+import { PencilSquareIcon} from '@heroicons/react/24/outline'
+
+
+
 type ActivityListProps = {
     activities: Activity[]
 }
@@ -28,8 +32,13 @@ export default function ActivityList({activities} : ActivityListProps) {
                         </p>
                     </div>
 
-                    <div>
+                    <div className=" flex gap-5 items-center">
+                        <button>
+                            <PencilSquareIcon
+                                className="h-8 w-8 text-gray-800"
 
+                            />
+                        </button>
                     </div>
 
                 </div>
