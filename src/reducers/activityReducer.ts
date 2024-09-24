d54cca97ce13ead1f19b39a22cf64f087903bb99
jorge <ogomez.isc@gmail.com>
2024-09-24 12:34:59 -0600

    Se habilito el boton de editar y se comprobo su funcionamiento

--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -23,9 +23,21 @@ export const activityReducer = (
     
     if(action.type === 'save-activity'){
         // Este codigo maneja la logica para actualizar el State
+
+        let updatedActivities : Activity[] = []
+        if(state.activeId) {
+            updatedActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity
+                : activity) 
+
+        } else {
+            updatedActivities = [...state.activities, action.payload.newActivity] 
+
+        }
+
         return {
            ...state,
-           activities: [...state.activities, action.payload.newActivity] 
+           activities: updatedActivities,
+           activeId: ''
         }
     }
 
