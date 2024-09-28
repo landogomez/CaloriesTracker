ca95ae321cd6429a6b3a835d32bd2eaa1cc6d822
jorge <ogomez.isc@gmail.com>
2024-09-28 12:57:24 -0600

    Se implemento un componente para mostrar las calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
 import { useMemo } from "react"
 import { Activity } from "../types"
+import CalorieDisplay from "./CalorieDisplay"
 
 type CalorieTrackerProps = {
   activities: Activity[]
@@ -21,16 +22,14 @@ export default function CalorieTracker({activities} : CalorieTrackerProps) {
       <h2 className="text-4xl font-black text-white text-center">Resumen de Calorías</h2>
 
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          
-          <span className="font-black text-6xl text-orange">{caloriesConsumed}</span>
-          Consumidas
-        </p>
-        <p className=" text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          
-          <span className="font-black text-6xl text-orange">{caloriesBurned}</span>
-          Quemadas
-        </p>
+        <CalorieDisplay 
+            calories={caloriesConsumed}
+            text="Consumidas"
+        />
+        <CalorieDisplay 
+            calories={caloriesBurned}
+            text="Quemadas"
+        />
       </div>
       
     </>
